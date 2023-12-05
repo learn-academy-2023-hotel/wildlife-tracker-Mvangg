a7b43f67be9451ce14736cb6a47ffea7cd88a098
mvangg <marivang@live.com>
2023-12-05 13:50:41 -0800

    story 1 down

---
+++ app/controllers/wildlives_controller.rb
@@ -0,0 +1,44 @@
+class WildlivesController < ApplicationController
+    def index
+        wildlives = Wildlife.all
+        render json: wildlives
+    end
+    def show
+        wildlife = Wildlife.find(params[:id])
+        if wildlife.valid?
+            render json: wildlife
+        else
+            render json: wildlife.errors
+        end
+    end
+    def create
+        wildlife = Wildlife.create(wildlife_params)
+        if wildlife.valid?
+            render json: wildlife
+        else
+            render json: wildlife.errors
+        end
+    end
+    def update
+        wildlife = Wildlife.find(params[:id])
+        wildlife.update(wildlife_params)
+        if wildlife.valid?
+            render json: wildlife
+        else
+            render json: wildlife.errors
+        end
+    end
+    def destroy
+        wildlife = Wildlife.find(params[:id])
+        if wildlife.destroy
+            render json: wildlife
+        else
+            render json: wildlife.errors
+        end
+    end
+
+    private
+    def wildlife_params
+        params.required(:wildlife).permit(:common_name, :scientific_binomial)
+    end
+end
